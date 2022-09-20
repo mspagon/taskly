dbe608233d99191f38d57efcf79bee27ef2f2326
Michael Spagon <mspagon@Michaels-MacBook-Pro.local>
2022-09-19 21:36:08 -0500

    Adds ability to view users and edit users in admin page.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,34 @@
-# from django.contrib import admin
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+# Future proofing for language translation.
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for user."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+
+admin.site.register(models.User, UserAdmin)
